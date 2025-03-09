8a1d55d86b1ea4ca04ba260bc1381d5e907316ad
Desmond Kofi Adusei <desdhi24@gmail.com>
2025-03-09 21:58:40 +0000

    chore: worked on the approved materials page

--- src/components/pages/supply-chain/warehouse/approved-raw-materials/index.tsx
+++ src/components/pages/supply-chain/warehouse/approved-raw-materials/index.tsx
@@ -1,10 +1,12 @@
 "use client";
 
-import { useRouter } from "next/navigation";
-import React, { useEffect, useState } from "react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import React, { useCallback, useEffect, useState } from "react";
 
 import PageWrapper from "@/components/layout/wrapper";
-import { useLazyGetApiV1MaterialApprovedRawMaterialsQuery } from "@/lib/redux/api/openapi.generated";
+import { EMaterialKind } from "@/lib";
+import { useLazyGetApiV1MaterialApprovedMaterialsQuery } from "@/lib/redux/api/openapi.generated";
+import AccessTabs from "@/shared/access";
 import { ServerDatatable } from "@/shared/datatable";
 import PageTitle from "@/shared/title";
 
@@ -12,8 +14,29 @@ import { columns } from "./columns";
 
 const Page = () => {
   const router = useRouter();
+  const searchParams = useSearchParams();
+  const kind = searchParams.get("kind") as unknown as EMaterialKind; // Extracts 'type' from URL
+
+  const pathname = usePathname();
+
+  // Get a new searchParams string by merging the current
+  // searchParams with a provided key/value pair
+  const createQueryString = useCallback(
+    (name: string, value: string) => {
+      const params = new URLSearchParams(searchParams.toString());
+      params.set(name, value);
+
+      return params.toString();
+    },
+    [searchParams],
+  );
+
+  const handleTabClick = (tabType: EMaterialKind) => {
+    router.push(pathname + "?" + createQueryString("kind", tabType.toString()));
+  };
+
   const [loadData, { data: result, isFetching, isLoading }] =
-    useLazyGetApiV1MaterialApprovedRawMaterialsQuery();
+    useLazyGetApiV1MaterialApprovedMaterialsQuery();
 
   const [pageSize, setPageSize] = useState(30);
   const [page, setPage] = useState(1);
@@ -21,21 +44,38 @@ const Page = () => {
     loadData({
       page,
       pageSize,
+      kind: kind || EMaterialKind.Raw,
     });
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [page, pageSize]);
+  }, [page, pageSize, kind]);
   const data = result?.data || [];
   return (
     <PageWrapper className="w-full space-y-2 py-1">
       <div className="flex items-center justify-between py-2">
         <PageTitle title="Approved Raw Materials" />
+        <div className="flex items-center justify-end gap-2">
+          <AccessTabs
+            handleTabClick={handleTabClick}
+            type={kind}
+            tabs={[
+              {
+                label: EMaterialKind[EMaterialKind.Raw],
+                value: EMaterialKind.Raw.toString(),
+              },
+              {
+                label: EMaterialKind[EMaterialKind.Package],
+                value: EMaterialKind.Package.toString(),
+              },
+            ]}
+          />
+        </div>
       </div>
 
       <ServerDatatable
         onRowClick={(row) => {
           router.push(
-            `/warehouse/approved-raw-materials/${row?.material?.id}/details`,
+            `/warehouse/approved-materials/${row?.material?.id}/details`,
           );
         }}
         data={data}
