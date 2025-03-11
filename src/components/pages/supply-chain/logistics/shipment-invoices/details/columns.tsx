4815179671206fe75d60737f10a9b0e764a2e29b
Joelisking <adujoel7@gmail.com>
2025-03-11 20:23:29 +0000

    chore: shipment invoice details page added

---
+++ src/components/pages/supply-chain/logistics/shipment-invoices/details/columns.tsx
@@ -0,0 +1,53 @@
+import { ColumnDef } from "@tanstack/react-table";
+
+import { Units, convertToLargestUnit } from "@/lib";
+import { ShipmentInvoiceItemDtoRead } from "@/lib/redux/api/openapi.generated";
+
+export const getColumns = (): ColumnDef<ShipmentInvoiceItemDtoRead>[] => [
+  {
+    accessorKey: "materialCode",
+    header: "Material Code",
+    cell: ({ row }) => <div>{row.original.material?.code ?? "-"}</div>,
+  },
+  {
+    accessorKey: "materialName",
+    header: "Material Name",
+    cell: ({ row }) => <div>{row.original.material?.name ?? "-"}</div>,
+  },
+  {
+    accessorKey: "manufacturerName",
+    header: "Manufacturer Name",
+    cell: ({ row }) => <div>{row.original.manufacturer?.name ?? "-"}</div>,
+  },
+  {
+    accessorKey: "orderQuantity",
+    header: "Order Quantity",
+    cell: ({ row }) => {
+      const qty = convertToLargestUnit(
+        row.original.receivedQuantity as number,
+        row.original.uoM?.symbol as Units,
+      );
+      return (
+        <div className="">
+          {qty.value.toFixed(2)} {qty.unit}
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "pricePerUnit",
+    header: "Price per Unit",
+    cell: ({ row }) => <div>{row.original.price ?? "-"}</div>,
+  },
+  {
+    accessorKey: "totalCost",
+    header: "Total Cost",
+    cell: ({ row }) => (
+      <div>
+        {row.original.price !== undefined
+          ? (row.original.receivedQuantity as number) * row.original.price
+          : "-"}
+      </div>
+    ),
+  },
+];
--- src/components/pages/supply-chain/logistics/shipment-invoices/details/index.tsx
+++ src/components/pages/supply-chain/logistics/shipment-invoices/details/index.tsx
@@ -1,18 +1,78 @@
 "use client";
 
-import { useParams } from "next/navigation";
-import React from "react";
+import { useParams, useRouter } from "next/navigation";
 
+import { Card, CardContent, CardTitle, Icon } from "@/components/ui";
 import { useGetApiV1ProcurementShipmentInvoiceByIdQuery } from "@/lib/redux/api/openapi.generated";
+import ScrollablePageWrapper from "@/shared/page-wrapper";
+import PageTitle from "@/shared/title";
 
-const Page = () => {
+import TableForData from "./table";
+
+const ShipmentInvoiceDetails = () => {
   const { id } = useParams();
   const shipmentInvoiceId = id as string;
+  const router = useRouter();
   const { data } = useGetApiV1ProcurementShipmentInvoiceByIdQuery({
     id: shipmentInvoiceId,
   });
-  console.log(data, "data");
-  return <div>Page</div>;
+
+  return (
+    <ScrollablePageWrapper>
+      <div
+        className="group mb-2 flex items-center gap-1 hover:cursor-pointer"
+        onClick={() => {
+          router.back();
+        }}
+      >
+        <Icon name="ArrowLeft" className="h-5 w-5" />
+        <div className="group-hover:underline">
+          <PageTitle title={"Shipment Invoice List"} />
+        </div>
+      </div>
+
+      <div className="my-5 space-y-3">
+        <Card className="my-8">
+          <CardContent className="space-y-4 py-2">
+            <div className="flex justify-start gap-4">
+              <div className="w-full space-y-2">
+                <span className="font-Medium block text-lg">
+                  Shipment Invoice Information
+                </span>
+                <div className="grid w-full grid-cols-3 gap-2">
+                  <div className="space-y-1">
+                    <span className="text-sm font-normal text-neutral-secondary">
+                      Shipment Invoice Code:{" "}
+                    </span>
+                    <span className="text-sm font-normal text-neutral-dark">
+                      {data?.code}
+                    </span>
+                  </div>
+
+                  <div className="space-y-1">
+                    <span className="text-sm font-normal text-neutral-secondary">
+                      Supplier Name:{" "}
+                    </span>
+                    <span className="text-sm font-normal text-neutral-dark">
+                      {data?.supplier?.name}
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+      <div className="space-y-3">
+        <Card className="space-y-4 p-5">
+          <CardTitle>
+            <span className="text-sm text-gray-500">Invoice Items</span>
+          </CardTitle>
+          <TableForData lists={data?.items || []} setItemLists={() => {}} />
+        </Card>
+      </div>
+    </ScrollablePageWrapper>
+  );
 };
 
-export default Page;
+export default ShipmentInvoiceDetails;
