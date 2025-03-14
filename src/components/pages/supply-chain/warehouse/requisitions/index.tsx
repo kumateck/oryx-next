35a775d6975fe41ddf800f8c20ab4f5b4543625f
Desmond Kofi Adusei <desdhi24@gmail.com>
2025-03-14 12:04:53 +0000

    chore: added raw and packaging to stock requistions

--- src/components/pages/supply-chain/warehouse/requisitions/index.tsx
+++ src/components/pages/supply-chain/warehouse/requisitions/index.tsx
@@ -1,18 +1,21 @@
 "use client";
 
-import { useRouter } from "next/navigation";
-import React, { useEffect, useState } from "react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import React, { useCallback, useEffect, useState } from "react";
 
 import PageWrapper from "@/components/layout/wrapper";
-import { RequisitionType } from "@/lib";
+import { EMaterialKind, RequisitionType } from "@/lib";
 import { useLazyGetApiV1RequisitionDepartmentQuery } from "@/lib/redux/api/openapi.generated";
+import AccessTabs from "@/shared/access";
 import { ServerDatatable } from "@/shared/datatable";
 import PageTitle from "@/shared/title";
 
 import { columns } from "./columns";
 
 const Page = () => {
+  const searchParams = useSearchParams();
   const router = useRouter();
+  const kind = searchParams.get("kind") as unknown as EMaterialKind; // Extracts 'type' from URL
   const [pageSize, setPageSize] = useState(30);
   const [page, setPage] = useState(1);
 
@@ -24,25 +27,50 @@ const Page = () => {
       page,
       pageSize,
       type: RequisitionType.StockVoucher,
+      kind: kind || EMaterialKind.Raw,
     });
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [page, pageSize]);
+  }, [page, pageSize, kind]);
 
   const data = result?.data || [];
+
+  const pathname = usePathname();
+
+  // Get a new searchParams string by merging the current
+  // searchParams with a provided key/value pair
+  const createQueryString = useCallback(
+    (name: string, value: string) => {
+      const params = new URLSearchParams(searchParams.toString());
+      params.set(name, value);
+
+      return params.toString();
+    },
+    [searchParams],
+  );
+
+  const handleTabClick = (tabType: EMaterialKind) => {
+    router.push(pathname + "?" + createQueryString("kind", tabType.toString()));
+  };
+
   return (
     <PageWrapper className="w-full space-y-2 py-1">
       <div className="flex items-center justify-between py-2">
-        <PageTitle title="Requisitions" />
-        {/* <div className="flex items-center justify-end gap-2">
-          <Button
-            variant="default"
-            size={"sm"}
-            onClick={() => router.push(routes.newRequisition())}
-          >
-            <Icon name="Plus" className="h-4 w-4" /> <span>Create</span>
-          </Button>
-        </div> */}
+        <PageTitle title="Stock Requisitions" />
+        <AccessTabs
+          handleTabClick={handleTabClick}
+          type={kind}
+          tabs={[
+            {
+              label: EMaterialKind[EMaterialKind.Raw],
+              value: EMaterialKind.Raw.toString(),
+            },
+            {
+              label: EMaterialKind[EMaterialKind.Packing],
+              value: EMaterialKind.Packing.toString(),
+            },
+          ]}
+        />
       </div>
 
       <ServerDatatable
