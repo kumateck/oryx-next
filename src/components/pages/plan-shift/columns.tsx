6037c144306a69b0ab281b3e58e123b5f311ac62
Desmond <desdhi24@gmail.com>
2025-05-26 15:27:14 +0000

    chore: make changes

---
+++ src/components/pages/plan-shift/columns.tsx
@@ -0,0 +1,67 @@
+"use client";
+import { ColumnDef, Row } from "@tanstack/react-table";
+import { ShiftFrequency, StartDay } from "@/lib";
+import { ShiftScheduleDtoRead } from "@/lib/redux/api/openapi.generated";
+
+import { Icon } from "@/components/ui";
+
+import Link from "next/link";
+
+interface DataTableRowActionsProps<TData> {
+  row: Row<TData>;
+}
+
+export function DataTableRowActions<TData extends ShiftScheduleDtoRead>({
+  row,
+}: DataTableRowActionsProps<TData>) {
+  return (
+    <section className="flex items-center justify-end gap-2">
+      <div className="flex cursor-pointer items-center justify-start gap-2">
+        <Link href={`/plan-shift/${row.id}/calendar`}>
+          <Icon
+            name="Calendar"
+            className="text-blue-500 h-5 w-5 cursor-pointer"
+          />
+        </Link>
+      </div>
+    </section>
+  );
+}
+
+export const columns: ColumnDef<ShiftScheduleDtoRead>[] = [
+  {
+    accessorKey: "name",
+    header: "Schedule name",
+    cell: ({ row }) => <div>{row.original.scheduleName}</div>,
+  },
+
+  {
+    accessorKey: "frequency",
+    header: "Frequency",
+    cell: ({ row }) => (
+      <div>{ShiftFrequency[row.original.frequency as ShiftFrequency]}</div>
+    ),
+  },
+
+  {
+    accessorKey: "department",
+    header: "Department",
+    cell: ({ row }) => <div>{row.original.department?.name}</div>,
+  },
+
+  {
+    accessorKey: "startDay",
+    header: "Start Day & Time",
+    cell: ({ row }) => (
+      <div>
+        {StartDay[row.original.startDate as StartDay]} @-{" "}
+        {row.original.startTime}
+      </div>
+    ),
+  },
+
+  {
+    id: "actions",
+    cell: ({ row }) => <DataTableRowActions row={row} />,
+  },
+];
