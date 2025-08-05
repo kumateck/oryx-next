b84f58e935658929ce8c8fbb7a750eda766982c0
leyroy <atiwensolomon@gmail.com>
2025-08-05 21:10:00 +0000

    feat: implemant edit stock requisition

--- src/components/pages/settings/equipment/details/tabs/finished-goods.tsx
+++ src/components/pages/settings/equipment/details/tabs/finished-goods.tsx
@@ -1,42 +1,42 @@
-// import { FinishedProduct } from "@/lib/redux/api/openapi.generated";
-// import { ListsTable } from "@/shared/datatable";
+import { ProductDtoRead } from "@/lib/redux/api/openapi.generated";
+import { ListsTable } from "@/shared/datatable";
 
-// interface IProps {
-//   data: FinishedProduct[];
-// }
+interface IProps {
+  data: ProductDtoRead[];
+}
 
-// export const FinishedGoods = ({ data }: IProps) => {
-//   if (!data) {
-//     return <div>No Data</div>;
-//   }
-//   return (
-//     <div>
-//       <ListsTable
-//         columns={[
-//           {
-//             accessorKey: "name",
-//             header: "Name",
-//           },
-//           {
-//             accessorKey: "uom",
-//             header: "Unit of Measurement",
-//             cell: (info) => info.row.original.uoM?.name,
-//           },
-//           {
-//             accessorKey: "dosageForm",
-//             header: "Dosage Form",
-//           },
-//           {
-//             accessorKey: "standardCost",
-//             header: "Cost",
-//           },
-//           {
-//             accessorKey: "sellingPrice",
-//             header: "Selling Price",
-//           },
-//         ]}
-//         data={data}
-//       />
-//     </div>
-//   );
-// };
+export const FinishedGoods = ({ data }: IProps) => {
+  if (!data) {
+    return <div>No Data</div>;
+  }
+  return (
+    <div>
+      <ListsTable
+        columns={[
+          {
+            accessorKey: "name",
+            header: "Name",
+          },
+          {
+            accessorKey: "uom",
+            header: "Unit of Measurement",
+            cell: (info) => info.row.original.baseUoM?.name,
+          },
+          {
+            accessorKey: "dosageForm",
+            header: "Dosage Form",
+          },
+          {
+            accessorKey: "standardCost",
+            header: "Cost",
+          },
+          {
+            accessorKey: "sellingPrice",
+            header: "Selling Price",
+          },
+        ]}
+        data={data}
+      />
+    </div>
+  );
+};
