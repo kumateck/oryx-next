2046dd400316228d50af88d3ccc63ce048bb71a7
leyroy <atiwensolomon@gmail.com>
2025-08-17 17:30:25 +0000

    fix: fix error on requisition page

--- src/components/pages/extrals/stock-requisition/edit.tsx
+++ src/components/pages/extrals/stock-requisition/edit.tsx
@@ -35,7 +35,8 @@ const Edit = ({ isOpen, onClose, details }: VendorFormProps) => {
     usePutApiV1ItemsStockRequisitionsByIdMutation();
   const [loadDepartments, { isLoading: loadingDepartments }] =
     useLazyGetApiV1DepartmentQuery();
-  const [loadItems, { isLoading: loadingItems }] = useLazyGetApiV1ItemsQuery();
+  const [loadItems, { data: items, isLoading: loadingItems }] =
+    useLazyGetApiV1ItemsQuery();
 
   const dispatch = useDispatch();
 
@@ -77,6 +78,7 @@ const Edit = ({ isOpen, onClose, details }: VendorFormProps) => {
     control,
     formState: { errors },
     reset,
+    setValue,
     handleSubmit,
   } = useForm<StockRequisitionDto>({
     resolver: CreateStockRequisitionValidator,
@@ -98,6 +100,13 @@ const Edit = ({ isOpen, onClose, details }: VendorFormProps) => {
     control,
     name: "items",
   });
+
+  const handleProductChange = (index: number, selected: { value: string }) => {
+    const item = items?.data?.find((p) => p.id === selected.value);
+    if (item) {
+      setValue(`items.${index}.itemCode`, item?.code ?? "");
+    }
+  };
 
   const onSubmit = async (data: StockRequisitionDto) => {
     try {
@@ -141,6 +150,7 @@ const Edit = ({ isOpen, onClose, details }: VendorFormProps) => {
             errors={errors}
             register={register}
             isLoading={loadingItems}
+            handleProductChange={handleProductChange}
             append={append}
             remove={remove}
             fields={fields}
