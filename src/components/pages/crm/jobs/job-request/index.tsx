33f0563326a58a72216c8ddccbe8139d41661ad7
leyroy <atiwensolomon@gmail.com>
2025-08-21 01:54:47 +0000

    fix: update material stp to use asynce options types

--- src/components/pages/crm/jobs/job-request/index.tsx
+++ src/components/pages/crm/jobs/job-request/index.tsx
@@ -1,13 +1,16 @@
+"use client";
 import PageWrapper from "@/components/layout/wrapper";
 import { Button, Icon } from "@/components/ui";
 import { useLazyGetApiV1JobRequestsQuery } from "@/lib/redux/api/openapi.generated";
 import { ClientDatatable } from "@/shared/datatable";
 import PageTitle from "@/shared/title";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Create from "./create";
 
 function Page() {
   const [loadJobRequests, { data: result, isLoading }] =
     useLazyGetApiV1JobRequestsQuery();
+  const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
     loadJobRequests({});
@@ -16,9 +19,10 @@ function Page() {
   const data = result || [];
   return (
     <PageWrapper>
+      {isOpen && <Create isOpen={isOpen} onClose={() => setIsOpen(false)} />}
       <div className="flex items-center justify-between gap-2 w-full">
         <PageTitle title="Job Request" />
-        <Button variant="outline" className="whitespace-nowrap flex gap-2">
+        <Button className="whitespace-nowrap flex gap-2">
           <Icon name="Plus" />
           <span>Create</span>
         </Button>
